c3920454b43c6ab22125ed98c00da5855b8435b9
Prateek Patnaik <prateekpatnaikcomp@gmail.com>
2026-06-11 23:42:37 +0530

    round robin multiple led blinking with certain delay

--- STM32_BLUE_PILL/sw-delay/main.c
+++ STM32_BLUE_PILL/sw-delay/main.c
@@ -4,16 +4,36 @@ void delay();
 
 void main(void) {
   RCC_APB2ENR |= (1 << 4);
+  RCC_APB2ENR |= (1 << 3);
 
   GPIOC_CRH &= ~(0xF << 20);
   GPIOC_CRH |= (2 << 20);
+  GPIOC_ODR |= (1 << 13);
+
+  GPIOC_CRH &= ~(0xF << 24);
+  GPIOC_CRH |= (2 << 24);
+  GPIOC_ODR |= (1 << 14);
+
+  GPIOB_CRL &= ~(0xF << 20);
+  GPIOB_CRL |= (0x2 << 20);
+  GPIOB_ODR |= (1 << 5);
+
+  GPIOB_CRH &= ~(0xF << 20);
+  GPIOB_CRH |= (0x2 << 20);
+  GPIOB_ODR |= (1 << 13);
 
   while(1) {
     delay();
     GPIOC_ODR ^= (1 << 13);
+    delay();
+    GPIOB_ODR ^= (1 << 5);
+    delay();
+    GPIOB_ODR ^= (1 << 13);
+    delay();  
+    GPIOC_ODR ^= (1 << 14);
   }
 }
 
 void delay(void) {
-  for (int i = 0; i < 1000000; i++);
+  for (int i = 0; i < 100000; i++);
 }
--- STM32_BLUE_PILL/sw-delay/mmap-regs.h
+++ STM32_BLUE_PILL/sw-delay/mmap-regs.h
@@ -1,3 +1,7 @@
 #define RCC_APB2ENR     *((volatile unsigned int *) 0x40021018)
+#define GPIOB_CRL       *((volatile unsigned int *) 0x40010C00)
+#define GPIOB_CRH       *((volatile unsigned int *) 0x40010C04)
+#define GPIOB_ODR       *((volatile unsigned int *) 0x40010C0C)
+
 #define GPIOC_CRH       *((volatile unsigned int *) 0x40011004)
 #define GPIOC_ODR       *((volatile unsigned int *) 0x4001100C)
